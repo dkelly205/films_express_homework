d3cfb1e8d5f3695ea565802efcfbdb7a5d86c096
daniel kelly <danieljkelly91@gmail.com>
2018-04-17 09:23:37 +0100

    homework

---
+++ controllers/films.js
@@ -0,0 +1,37 @@
+//since we don't have a database we'll use our front end models at the moment
+var films = require('../client/src/models/films')();
+var Film = require('../client/src/models/film');
+var Review = require('../client/src/models/review');
+
+var express = require('express');
+var filmRouter = new express.Router();
+
+
+filmRouter.get('/:id', function(req, res){
+  res.json({data: films[req.params.id]});
+})
+
+filmRouter.get('/', function(req, res) {
+  res.json({data: films});
+})
+
+
+//create
+filmRouter.post('/', function(req, res){
+  films.push(req.body.film);
+  res.json({data: films});
+})
+
+//update
+filmRouter.put('/:id', function(req, res){
+  films[req.params.id] = req.body.film;
+  res.json({data: films});
+})
+
+//delete
+filmRouter.delete('/:id', function(req, res){
+  films.splice(req.params.id, 1);
+  res.json({data:films});
+})
+
+module.exports = filmRouter;
